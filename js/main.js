82358442ad7acbddcefd1d2dd3320be904646280
Devine <g_devine@tss-hr420-dm15.fc.ca>
2018-10-17 11:35:55 -0400

    updated js file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 (()=> {
-fuction fetchData(){
-    fetch('../includes/connect.php?carModel=R58')
-    .then(res = res.json())
-    .then(data => console.log(data))
-    .catch(function(error){
-        console.error(error);
-    });
-}
+    function fetchData() {
+        fetch('./includes/connect.php?carModel=R58')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(function(error) {
+            console.error(error);
+        });
+    }
 
-
+    fetchData();
 })();
