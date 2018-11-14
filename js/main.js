97918fda4d85fd9868c823ef69a4e42369c9693b
Devine <g_devine@tss-hr420-dm15.local>
2018-11-14 10:34:18 -0500

    making changed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 (()=> {
-    function fetchData() {
-        fetch('./includes/connect.php?carModel=R58')
+
+    const vm = new VTTCue({
+
+        el : "#app",
+        data: {
+            modelname: "",
+            modelpricing : "",
+            modeldetails : ""
+        },
+    })
+    mounted : function() {
+        console.log('mounted');
+
+        document.querySelector("#F55").click();
+    },
+
+    updated : function() {
+        console.log('updated');
+    },
+    methods: {
+    fetchData(e) {
+
+        //gets the id of the elements via the events object
+
+        let targetURL = e.currentTarget.id;
+
+        fetch(`./includes/connect.php?carModel=${targetURL}`)
         .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(function(error) {
+        .then(data => {
+            console.log(data);
+            const { modelName, Pricing, modelDetails }= data[0];
+
+            this.modelname = modelName;
+            this.modeldetails = modelDetails;
+            this.modelpricing = pricing;
+        })
+        .catch(function(error){
             console.error(error);
         });
     }
+ }
 
     fetchData();
 })();
